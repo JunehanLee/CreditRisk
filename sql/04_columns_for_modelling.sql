9630b032b4d69437328cf19b062c61e9ae743b9a
JunehanLee <30474870+JunehanLee@users.noreply.github.com>
2026-03-25 12:21:00 +0000

    Update 04_columns_for_modelling.sql

--- sql/04_columns_for_modelling.sql
+++ sql/04_columns_for_modelling.sql
@@ -10,7 +10,15 @@ SELECT
        Applicant Info
     ========================= */
     annual_inc,
-    emp_length_years,
+    CASE
+        WHEN emp_length_years IS NULL THEN '__NULL__'
+        WHEN emp_length_years = 0 THEN '0'
+        WHEN emp_length_years BETWEEN 1 AND 2 THEN '1-2'
+        WHEN emp_length_years BETWEEN 3 AND 5 THEN '3-5'
+        WHEN emp_length_years BETWEEN 6 AND 9 THEN '6-9'
+        ELSE '10+'
+    END AS emp_length_years,
+
     home_ownership,
     verification_status,
     purpose,
@@ -29,9 +37,27 @@ SELECT
     total_bc_limit,
     bc_util_pct,
     revol_util_pct,
-    mort_acc,
-    inq_last_6mths,
-    num_accts_ever_120_pd,
+
+    CASE
+        WHEN mort_acc IS NULL THEN '__NULL__'
+        WHEN mort_acc = 0 THEN '0'
+        WHEN mort_acc = 1 THEN '1'
+        ELSE '2+'
+    END AS mort_acc,
+
+    CASE
+        WHEN inq_last_6mths IS NULL THEN '__NULL__'
+        WHEN inq_last_6mths = 0 THEN '0'
+        WHEN inq_last_6mths = 1 THEN '1'
+        ELSE '2+'
+    END AS inq_last_6mths,
+
+    CASE
+        WHEN num_accts_ever_120_pd IS NULL THEN '__NULL__'
+        WHEN num_accts_ever_120_pd = 0 THEN '0'
+        WHEN num_accts_ever_120_pd = 1 THEN '1'
+        ELSE '2+'
+    END AS num_accts_ever_120_pd,
 
     /* =========================
        Benchmark (NOT for training)
@@ -44,5 +70,4 @@ FROM CURATED_DATA.LOAN_CLEAN
 /* =========================
    Closed Loans Only
 ========================= */
-WHERE y_default IS NOT NULL
-;
+WHERE y_default IS NOT NULL;
